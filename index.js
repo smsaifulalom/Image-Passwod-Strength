9237126d9041475bc9be07b678356e8621732f91
smsaifulalom <35555808+smsaifulalom@users.noreply.github.com>
2021-02-14 10:03:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const password = document.getElementById('password')
-const background = document.getElementById('background')
+// const password = document.getElementById('password')
+// const background = document.getElementById('background')
 
-password.addEventListener('input',(e)=>{
+// password.addEventListener('input',(e)=>{
+//     const input = e.target.value
+//     const length = input.length
+//     const blurValue = 20 - length * 2
+//     background.style.filter = `blur(${blurValue}px)`
+// })
+
+const background = document.getElementById('background')
+const password = document.getElementById('password')
+
+password.addEventListener("input",(e)=>{
     const input = e.target.value
     const length = input.length
     const blurValue = 20 - length * 2
